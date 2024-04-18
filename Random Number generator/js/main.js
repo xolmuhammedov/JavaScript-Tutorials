09e9154dd21ae17c00b464f72db5fbde8fedf718
MuhammadYusuf <136478219+xolmuhammedov@users.noreply.github.com>
2024-04-18 21:41:35 +0500

    Random Number Generator

---
+++ Random Number generator/js/main.js
@@ -0,0 +1,10 @@
+const MyButton = document.getElementById("MyButton");
+const MyLabel = document.getElementById("MyLabel");
+const min = 1;
+const max = 100; //maksimal raqamni hohlagan raqamga o'zgratrishi mumkun!!!
+let randomNum;
+
+MyButton.onclick = function () {
+  randomNum = Math.floor(Math.random() * max) + min;
+  MyLabel.textContent = randomNum;
+};
