320aac5807684e96433f0acbfe4b979af4259626
MuhammadYusuf <136478219+xolmuhammedov@users.noreply.github.com>
2024-04-19 14:58:30 +0500

    Type Conversion

---
+++ Type changing/main.js
@@ -0,0 +1,20 @@
+let x = "pizza";
+let y = "pizza";
+let z = "pizza";
+
+x = Number(x);
+y = String(y);
+z = Boolean(z);
+
+console.log(x, typeof x);
+console.log(y, typeof y);
+console.log(z, typeof z);
+////////////////////////////
+
+let a = `0`;
+parseInt(a);
+/////////////////////////////
+
+let b = "0";
+b = +b;
+console.log(b, typeof b);
