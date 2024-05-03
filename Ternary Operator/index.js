82b578255e22781ee50efe1839437163b4211d8c
MuhammadYusuf <xolmuhammedovv@gmail.com>
2024-05-03 18:08:02 +0500

    progress

---
+++ Ternary Operator/index.js
@@ -0,0 +1,8 @@
+//ternary operator  = a shortcut to if() and else{} statements
+// helps to assign a variable based on a condition
+// condition ? codeIfTrue : codeIffalse
+
+let age = 21;
+let message = age >= 19 ? "You are an adult" : "You're a minor";
+//this is an alternative code like this
+
