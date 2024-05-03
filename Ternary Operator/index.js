032d2d0da37be27bb4a595348c48d7c886672688
MuhammadYusuf <xolmuhammedovv@gmail.com>
2024-05-03 18:09:56 +0500

    progress

--- Ternary Operator/index.js
+++ Ternary Operator/index.js
@@ -4,5 +4,14 @@
 
 let age = 21;
 let message = age >= 19 ? "You are an adult" : "You're a minor";
-//this is an alternative code like this
+//this is an alternative code like this ^^^^^^^^^
+
+let agee = 21;
+let messagee;
+if(age >= 18){
+    message = "You're an adult"
+}
+else{
+    message = "You're a minor"
+}
 
