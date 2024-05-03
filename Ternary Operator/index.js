f730b5945c6c1b4b4bf056aa5674cfa0111c5ad2
MuhammadYusuf <xolmuhammedovv@gmail.com>
2024-05-03 19:26:20 +0500

    Ternary Operator

--- Ternary Operator/index.js
+++ Ternary Operator/index.js
@@ -1,29 +1,29 @@
-//ternary operator  = a shortcut to if() and else{} statements
+// ternary operator  = a shortcut to if() and else{} statements
 // helps to assign a variable based on a condition
 // condition ? codeIfTrue : codeIffalse
 
-// let age = 21;
-// let message = age >= 19 ? "You are an adult" : "You're a minor";
-// console.log(message)
-//this is an alternative code like this ^^^^^^^^^
+let age = 21;
+let message = age >= 19 ? "You are an adult" : "You're a minor";
+console.log(message)
+// this is an alternative code like this ^^^^^^^^^
 
-// let agee = 21;
-// let messagee;
-// if(age >= 18){
-//     message = "You're an adult"
-// }
-// else{
-//     message = "You're a minor"
-// }
+let agee = 21;
+let messagee;
+if(age >= 18){
+    messagee = "You're an adult"
+}
+else{
+    message = "You're a minor"
+}
 
-// let time = 16;
-// let greeting = time < 12 ? "Godd morning!" : "Good afternoon";
-// console.log(greeting);
+let time = 16;
+let greeting = time < 12 ? "Godd morning!" : "Good afternoon";
+console.log(greeting);
 
 
-// let isStudent = false;
-// let message = isStudent ? "You are a student" : "You are NOT a student";
-// console.log(message);
+let isStudent = false;
+let messageee = isStudent ? "You are a student" : "You are NOT a student";
+console.log(messageee);
 
 
 
