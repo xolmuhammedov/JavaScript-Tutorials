d0686307127ed88d65c2ec74e26c38bc710e5d23
MuhammadYusuf <xolmuhammedovv@gmail.com>
2024-05-04 15:07:19 +0500

    Switches

---
+++ Switches/index.js
@@ -0,0 +1,30 @@
+//SWITCH = can be am efficent replacement to many else if statements
+
+let day = `pizza`;
+switch (day) {
+    case 1:
+        console.log("It is Monday")
+        break;
+    case 2:
+            console.log("It is Tuesday")
+            break;
+    case 3:
+            console.log("It is Wednesday")
+            break;
+        case 4:
+            console.log("It is Thursday")
+            break;
+    case 5:
+            console.log("It is Friday")
+            break;
+    case 6:
+            console.log("It is Saturday")
+            break;
+    case 6:
+            console.log("It is Sunday")
+            break;
+    default:
+        console.log(`${day} is not a day`); //default stands for else If the case are not mached we see the default;
+}
+
+// !!! it is important to put break after every case but if we don't put it every case will work it won't stop
