56dd44486f15331bd7f98c15fb61e99d3249d3bd
MuhammadYusuf <xolmuhammedovv@gmail.com>
2024-05-04 15:15:07 +0500

    Switches

--- Switches/index.js
+++ Switches/index.js
@@ -1,6 +1,6 @@
 //SWITCH = can be am efficent replacement to many else if statements
 
-let day = `pizza`;
+let day = `1`;
 switch (day) {
     case 1:
         console.log("It is Monday")
@@ -28,3 +28,32 @@ switch (day) {
 }
 
 // !!! it is important to put break after every case but if we don't put it every case will work it won't stop
+
+
+
+
+
+
+/// TEST score checker
+
+let testScore = 40
+let letterGrade;
+
+switch(true){
+    case testScore >= 90:
+        letterGrade = "A"
+        break;
+    case testScore >= 80:
+            letterGrade = "B"
+            break;
+    case testScore >= 70:
+            letterGrade = "C"
+            break;
+    case testScore >= 60:
+            letterGrade = "D"
+            break; 
+    default:
+        letterGrade = "F"                     
+}
+
+console.log(letterGrade)
