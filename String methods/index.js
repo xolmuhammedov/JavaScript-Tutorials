7f4eb193e60ad2ee290f8326d6c814b169a4e0aa
MuhammadYusuf <xolmuhammedovv@gmail.com>
2024-05-07 01:28:50 +0500

    toUp toLow

--- String methods/index.js
+++ String methods/index.js
@@ -6,4 +6,5 @@ let userName = "BroCode    ";
 //console.log(userName.indexOf("C")) // elemntni indexiga qarab topib beradi
 //console.log(userName.length) // uzunligini topib beradi
 
-console.log(userName.trim()) // boshidan yoki ohiridan tab larni olib beradi
+//console.log(userName.trim()) // boshidan yoki ohiridan tab larni olib beradi
+console.log(userName.toUpperCase()); console.log(userName.toLocaleLowerCase())
