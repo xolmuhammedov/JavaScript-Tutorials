2f1a64c606e5700da31e16ab209a00afa09bd225
MuhammadYusuf <xolmuhammedovv@gmail.com>
2024-05-07 12:34:13 +0500

    padStrart

--- String methods/index.js
+++ String methods/index.js
@@ -1,10 +1,23 @@
 
 // string metjods allows you to manipulate  and work with text (strings)
 
-let userName = "BroCode    ";
+let userName = " BroCode    ";
 //console.log(userName.charAt(1)) korstilgan elemnti chiqaradi
 //console.log(userName.indexOf("C")) // elemntni indexiga qarab topib beradi
 //console.log(userName.length) // uzunligini topib beradi
 
 //console.log(userName.trim()) // boshidan yoki ohiridan tab larni olib beradi
-console.log(userName.toUpperCase()); console.log(userName.toLocaleLowerCase()) // elementni katta yoki kichik harfga ozgartirish
+//console.log(userName.toUpperCase()); console.log(userName.toLocaleLowerCase()) // elementni katta yoki kichik harfga ozgartirish
+//console.log(userName.repeat(5)) // qaytarish 
+//console.log(userName.startsWith(" ")); console.log(userName.endsWith()tart)// nima bilan boshlanishini tekshirish
+// if (userName.startsWith(" ")) {
+//     alert('true')
+// }else{
+//     alert('false')
+// }\
+
+
+let phoneNumber = "123-456-7890"
+
+phoneNumber = phoneNumber.padStart(15 , "0")
+console.log(phoneNumber)
