025a3ddf1b87bdc892e725bf32e8eb4cf6f40576
MuhammadYusuf <xolmuhammedovv@gmail.com>
2024-05-08 20:10:16 +0500

    hii

---
+++ Constant/index.js
@@ -0,0 +1,11 @@
+    //const = a variable that can't be changed
+
+    let pi = 3.14159;
+    let radius;
+    let circumference;
+
+    radius = window.prompt("Enter the radius of a circle");
+    radius = Number(radius)
+
+    circumference = 2 * pi * radius;
+    console.log(circumference)
