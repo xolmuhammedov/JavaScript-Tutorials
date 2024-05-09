0fac615fe999eb4227e8ec1a7375096b1f8e633d
MuhammadYusuf <xolmuhammedovv@gmail.com>
2024-05-09 06:17:25 +0500

    using for loop

--- Series/index.js
+++ Series/index.js
@@ -8,12 +8,29 @@ const seriesDB = {
     private: false,
 };
 
-const a = prompt("Oxirgi ko'rgan serialingiz?"),
-b = prompt("Nechi baxo berasiz?"),
-c = prompt("Oxirgi ko'rgan serialingiz?"),
-d = prompt("Nechi baxo berasiz?");
 
-seriesDB.series[a] = b
-seriesDB.series[c] = d
+for (let i = 0; i < 2 ; i++){
+    const a = prompt("Oxirgi ko'rgan serialingiz?"),
+    b = prompt("Nechi baxo berasiz?");
+
+    if(a != null && b != null && a != "" && b != ""){
+        seriesDB.series[a] = b;
+        console.log("Done");
+    }else{
+        console.log("ERROR")
+        i--
+    }
+
+}
+
+if (seriesDB.count < 5) {
+    console.log("Kam serial ko'ribsiz")
+}else if(seriesDB.count >= 5 && seriesDB.count < 10){
+    console.log("Siz klassik tomoshabin ekansiz")
+}else if (seriesDB.count >= 10 ){
+    console.log("Siz serialchi zvezda ekansiz");
+}else{
+    console.log("Error");
+}
 
 console.log(seriesDB)
