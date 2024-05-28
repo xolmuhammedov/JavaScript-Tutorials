081cbce757253c12d0313c3178de51712e94202d
MuhammadYusuf <xolmuhammedovv@gmail.com>
2024-05-28 20:54:01 +0500

    undersatand classList and style

---
+++ addEventListener/script.js
@@ -0,0 +1,12 @@
+const cube = document.querySelector(".block");
+
+cube.onclick = function changeCube() {
+    cube.style.backgroundColor = "blue";
+}
+changeCube();
+
+
+if(cube == onclick){
+    cube.style.backgroundColor = "black";
+}else
+console.log(false , "brother")
