21334f5423a5de5da36850850ccccc378c3a6468
MuhammadYusuf <xolmuhammedovv@gmail.com>
2024-05-30 22:30:33 +0500

    string

--- String Slicing/main.js
+++ String Slicing/main.js
@@ -7,3 +7,9 @@ let firstName = fullName.slice(0 , 3);
 
 let lastName = fullName.slice(4 , 8);
 console.log(firstName , lastName);
+let text = document.querySelectorAll("a");
+let arr = ['hello' , 'uzbekistan' , 'world'];
+arr.forEach((element) => console.log(element));
+
+text.append(element);
+
