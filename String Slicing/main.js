54a022378694063693f6d2b9d274aacd0d7d2cc3
MuhammadYusuf <xolmuhammedovv@gmail.com>
2024-05-30 14:31:21 +0500

    slicing

---
+++ String Slicing/main.js
@@ -0,0 +1,9 @@
+// string slicing = creating a substring 
+// from a portion of another string
+//      string.slice(start, end)
+const fullName = "Bro Code";
+
+let firstName = fullName.slice(0 , 3);
+
+let lastName = fullName.slice(4 , 8);
+console.log(firstName , lastName);
