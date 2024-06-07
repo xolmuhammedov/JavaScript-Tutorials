fe5ffdcec803a3ebe26fdc0bcdfe6aa7f8a236bb
MuhammadYusuf <xolmuhammedovv@gmail.com>
2024-06-08 01:57:55 +0500

    what we gona do?

--- addEventListener/script.js
+++ addEventListener/script.js
@@ -1,12 +1,1 @@
-const cube = document.querySelector(".block1");
-const cube2 = document.querySelector(".block2");
-const cube3 = document.querySelector(".block3");
-
-// cube.onclick = function changeCube() {
-//     cube.style.backgroundColor = "blue";
-// }
-// changeCube();
-
-cube.addEventListener('click' , () => {
-    cube.classList.add(cube2);
-})
+console.log("what we gonna do?");
