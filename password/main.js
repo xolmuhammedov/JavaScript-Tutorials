3f61cd5d72b05421aed25ecdbf5512266dbc6e08
MuhammadYusuf <xolmuhammedovv@gmail.com>
2024-06-23 20:17:13 +0500

    so password stuff

---
+++ password/main.js
@@ -0,0 +1,20 @@
+const userPrompt = prompt("Write your username!");
+const userPassword = prompt("Write your password!");
+
+const username = "admin";
+const password = 123;
+
+const data = {
+    user: "admin2",
+    pass: 1234.
+};
+
+if (userPrompt == data.user & userPassword == data.pass) {
+    alert("Correct!");
+    
+}else{
+    alert("Try again!")
+}
+
+
+//userPrompt == username & userPassword == password ? alert("hi") : alert("sf");
